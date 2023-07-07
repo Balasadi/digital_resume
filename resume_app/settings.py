9766824defe8698aab6c298e82fd50be475b9a2b
Balasadi <110322911+Balasadi@users.noreply.github.com>
2023-07-07 14:49:29 -0700

    Update settings.py

--- resume_app/settings.py
+++ resume_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+# Configure Django App for Heroku.
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +143,4 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+django_heroku.settings(locals())
